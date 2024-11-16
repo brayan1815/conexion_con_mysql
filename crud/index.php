6fb08d2873d90305fa5ad9abaffcef104edfdc76
Brayan <brayanfernandezjimenez18@gmail.com>
2024-11-16 11:49:19 -0500

    fn

--- crud/index.php
+++ crud/index.php
@@ -9,9 +9,22 @@ $sql="SELECT * FROM ciudades";
 $bandera=$conexion->prepare($sql);
 $bandera->execute();
 $ciudades= $bandera->fetchAll();
-
-echo"<pre>";
-print_r($ciudades);
-echo"</pre>";
 ?>
 
+<form action="" method="post">
+    <div>
+        <label for="ciudad_id">Ciudad</label>
+        <select name="ciudad_id" id="ciudad_id">
+            <?php
+            foreach($ciudades as $key => $value){
+            ?>
+                <option id="<?=$value['id_ciudad']?>">
+                    <?=$value['ciudad']?>
+                </option>
+            <?php
+            }
+            ?>
+            }
+        </select>
+    </div>
+</form>
